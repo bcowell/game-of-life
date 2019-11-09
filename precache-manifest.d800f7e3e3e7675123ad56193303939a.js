2a994841d9e62d9bffc5cec32e300df40d9c1f21
Brayden Cowell <bcowell@hotmail.ca>
2019-11-08 20:50:34 -0500

    Updates

---
+++ precache-manifest.d800f7e3e3e7675123ad56193303939a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  },
+  {
+    "revision": "c53d716e6d0f7cec084d",
+    "url": "/game-of-life/static/js/main.14f5c928.chunk.js"
+  },
+  {
+    "revision": "a020c2e4dac8a47ca5b4",
+    "url": "/game-of-life/static/js/2.526fa1d5.chunk.js"
+  },
+  {
+    "revision": "c53d716e6d0f7cec084d",
+    "url": "/game-of-life/static/css/main.53ea099d.chunk.css"
+  },
+  {
+    "revision": "a194d33f94acecee3377fa6b5b95dcfc",
+    "url": "/game-of-life/index.html"
+  }
+];
